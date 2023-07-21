069ca43e014b870c1957daaf7aad6e19b8b50948
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-07-21 15:05:45 +0200

    js: add Fizz, Buzz and FizzBuzz

--- js/main.js
+++ js/main.js
@@ -3,4 +3,13 @@ const list = document.getElementById("list");
 for (let i = 1; i <= 100; i++) {
   console.log(i);
   list.innerHTML += `<li>${i}</li>`;
+  if (i % 3 == 0) {
+    list.innerHTML += `<li>Fizz</li>`;
+  }
+  if (i % 5 == 0) {
+    list.innerHTML += `<li>Buzz</li>`;
+  }
+  if ((i % 5 == 0) + (i % 3 == 0)) {
+    list.innerHTML += `<li>BuzzFizz</li>`;
+  }
 }
