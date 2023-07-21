bd5a779262fbf3af2121429b98a08a088ce2fa43
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-07-21 15:37:38 +0200

    js: edit

--- js/main.js
+++ js/main.js
@@ -2,14 +2,12 @@ const list = document.getElementById("list");
 
 for (let i = 1; i <= 100; i++) {
   console.log(i);
-  list.innerHTML += `<li>${i}</li>`;
+
   if (i % 3 == 0) {
     list.innerHTML += `<li>Fizz</li>`;
-  }
-  if (i % 5 == 0) {
+  } else if (i % 5 == 0) {
     list.innerHTML += `<li>Buzz</li>`;
-  }
-  if ((i % 5 == 0) + (i % 3 == 0)) {
-    list.innerHTML += `<li>BuzzFizz</li>`;
-  }
+  } else if ((i % 3 == 0) + (i % 5 == 0)) {
+    list.innerHTML += `<li>FizzBuzz</li>`;
+  } else list.innerHTML += `<li>${i}</li>`;
 }
